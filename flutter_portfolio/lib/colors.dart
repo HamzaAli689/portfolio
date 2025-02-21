4988cfc6bc2b79c675dbf21f18ef2eeae44f92ec
Hamza Ali <hamzaalivr33@gmail.com>
2025-02-21 21:56:53 +0500

    create colors.dart file

---
+++ flutter_portfolio/lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+
+   const Color primary = Color(0xFFD84040); // Main Red
+   const Color secondary = Color(0xFFA31D1D); // Dark Red
+   const Color background = Color(0xFFECDCBF); // Light Beige
+   const Color accent = Color(0xFFF8F2DE); // Soft Cream
+
