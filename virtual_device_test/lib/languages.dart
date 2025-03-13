f1d2949a9958f5e28e1b7b489133e0b166dc57c7
Hamza Ali <hamzaalivr33@gmail.com>
2025-03-13 15:14:10 +0500

    create languages.dart file

---
+++ virtual_device_test/lib/languages.dart
@@ -0,0 +1,15 @@
+import 'package:get/get_navigation/src/root/internacionalization.dart';
+
+class Languages extends Translations{
+  @override
+  // TODO: implement keys
+  Map<String, Map<String, String>> get keys => {
+    "en_US": {
+      "Massage": "What is your name?",
+      "Name": "Hamza Ali",
+    },
+
+
+  };
+
+}
