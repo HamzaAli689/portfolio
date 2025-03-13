64595cd0d32e486b35df62b6b10741639d336a82
Hamza Ali <hamzaalivr33@gmail.com>
2025-03-13 15:53:27 +0500

    update the mai9n.dart file

--- virtual_device_test/lib/main.dart
+++ virtual_device_test/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:virtual_device_test/languages.dart';
-import 'package:virtual_device_test/testfile.dart';
+
+import 'homeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,61 +17,61 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      translations: Languages(),
-      locale: Locale("en","US"),
+      // translations: Languages(),
+      // locale: Locale("en","US"),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  Testfile(),
+      home:  Homescreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key,});
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key,});
+//
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Hamza Ali"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              SizedBox(height: 20,),
-              const Text(
-                "Show Date",
-              ),
-              ElevatedButton(onPressed: () async{
-                DateTime? pickDate = await showDatePicker(context: context,initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2026),
-                );
-
-                if(pickDate != null){
-                  print("this ois slected date: ${pickDate.year} - ${pickDate.month} - ${pickDate.day}");}
-                print(pickDate);
-              }, child: Text("Select Date")),
-              ElevatedButton(onPressed: () async{
-                TimeOfDay? pickTime = await showTimePicker(context: context, initialTime: TimeOfDay.now());
-                print("this is my slected time: ${pickTime?.hour} - ${pickTime?.minute}");
-              }, child: Text("Select Time")),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//           title: Text("Hamza Ali"),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//
+//               SizedBox(height: 20,),
+//               const Text(
+//                 "Show Date",
+//               ),
+//               ElevatedButton(onPressed: () async{
+//                 DateTime? pickDate = await showDatePicker(context: context,initialDate: DateTime.now(),
+//                     firstDate: DateTime(2000),
+//                     lastDate: DateTime(2026),
+//                 );
+//
+//                 if(pickDate != null){
+//                   print("this ois slected date: ${pickDate.year} - ${pickDate.month} - ${pickDate.day}");}
+//                 print(pickDate);
+//               }, child: Text("Select Date")),
+//               ElevatedButton(onPressed: () async{
+//                 TimeOfDay? pickTime = await showTimePicker(context: context, initialTime: TimeOfDay.now());
+//                 print("this is my slected time: ${pickTime?.hour} - ${pickTime?.minute}");
+//               }, child: Text("Select Time")),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
