03f575717b450dc3674dabffa035ccf26a66116c
Hamza Ali <hamzaalivr33@gmail.com>
2025-03-13 15:52:04 +0500

    update the languages.dart file

--- virtual_device_test/lib/languages.dart
+++ virtual_device_test/lib/languages.dart
@@ -1,19 +1,19 @@
-import 'package:get/get_navigation/src/root/internacionalization.dart';
-
-class Languages extends Translations{
-  @override
-  // TODO: implement keys
-  Map<String, Map<String, String>> get keys => {
-    "en_US": {
-      "Massage": "What is your name?",
-      "Name": "Hamza Ali",
-    },
-    "ur_PK": {
-      "Massage": "آپ کا نام کیا ہے؟",
-      "Name": "حمزہ علی",
-    },
-
-
-  };
-
-}
+// import 'package:get/get_navigation/src/root/internacionalization.dart';
+//
+// class Languages extends Translations{
+//   @override
+//   // TODO: implement keys
+//   Map<String, Map<String, String>> get keys => {
+//     "en_US": {
+//       "Massage": "What is your name?",
+//       "Name": "Hamza Ali",
+//     },
+//     "ur_PK": {
+//       "Massage": "آپ کا نام کیا ہے؟",
+//       "Name": "حمزہ علی",
+//     },
+//
+//
+//   };
+//
+// }
