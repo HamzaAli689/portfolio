95c77e21025168eb0376479aafd952a2579e8a37
Hamza Ali <hamzaalivr33@gmail.com>
2025-03-15 17:42:28 +0500

    update the main.dart file

--- print_arabic/lib/main.dart
+++ print_arabic/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
-import 'BluetoothScreen.dart';
-import 'PrintScreen.dart';
+
+import 'homePage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Thermal Printer App',
-      home: BluetoothScreen(),
-      routes: {
-        '/print': (context) => PrintScreen(),
-      },
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home:  Homepage(),
     );
   }
 }
+
