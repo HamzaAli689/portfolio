0a7eab39c7132f57b8da689093e0c6e07c314ce6
Hamza Ali <hamzaalivr33@gmail.com>
2025-03-28 22:41:08 +0500

    complete the app

--- virtual_device_test/lib/favorit_app/faovratApp.controller.dart
+++ virtual_device_test/lib/favorit_app/faovratApp.controller.dart
@@ -4,7 +4,12 @@ class FaovratController extends GetxController{
   RxList<String> fruits = ["Apple", "Mango", "Banana", "Orange"].obs;
   RxList favorit = [].obs;
 
-
+void addFavorit(String value){
+  favorit.add(value);
+}
+  void removeFavorit(String value){
+    favorit.remove(value);
+  }
 
 
 }
