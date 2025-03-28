6958146f2ea612150618135d177c19536dcf8c63
Hamza Ali <hamzaalivr33@gmail.com>
2025-03-28 22:33:05 +0500

    create the controller of favorit app

---
+++ virtual_device_test/lib/favorit_app/faovratApp.controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class FaovratController extends GetxController{
+  RxList<String> fruits = ["Apple", "Mango", "Banana", "Orange"].obs;
+  RxList favorit = [].obs;
+
+
+
+
+}
